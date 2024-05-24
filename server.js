6acaaefccee43bc3613b8d0f363319c33840e340
Adeyemi Adedapo <adedapo025@gmail.com>
2024-05-24 18:51:12 +0100

    JSON file as storage implemented

--- server.js
+++ server.js
@@ -1,46 +1,43 @@
 const http = require('node:http')
-const fs = require('fs').promises
+const fs = require('fs')
 const querystring = require('querystring')
 
 const server = http.createServer((req, res) => {
     // Loads the HTML file using the fs module
-    fs.readFile(__dirname + '/index.html')
-        .then(data =>{
-            res.writeHead(200, {'Content-Type': 'text/html'})
-            res.end(data)
-        })    
-        .catch(err => {
-            res.writeHead(500)
-            res.end(err)
-        })
+    if (req.url === '/' && req.method === 'GET'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        const readStream = fs.createReadStream(__dirname + '/index.html', 'utf8')
+        readStream.pipe(res)
+    }
+    
 
-    // Handles POST request by submit button
-    if (req.url === '/submit-form' && req.method === 'POST'){
+//    Handles POST request by submit button
+    else if (req.url === '/submit-form' && req.method === 'POST'){
         let body = ''
         req.on('data', chunk => {
             body += chunk.toString()
         })    
         req.on('end', () =>{
             const formData = querystring.parse(body)
-
-            fs.readFile(__dirname + '/db/database.json', (err, data) =>{
+            
+            // Get file content if any, adds new form data into it then write back to JSON file
+            fs.readFile(__dirname + '/db/database.json', 'utf8', (err, data) =>{
                 let db = []
                 if (data){
                     db = JSON.parse(data)
                 }
-                db.push(formData)
+                db.push(formData) 
 
-                fs.writeFile(__dirname + '/db/database.json', JSON.stringify(db), (err) =>{
-                    if (err) throw err;
-                    console.log("Form data submitted successfully!")
+                fs.writeFile(__dirname + '/db/database.json', JSON.stringify(db, null, 1), err => {
+                    if (err) throw err
                 })
             })
-            // fs.appendFile(__dirname + '/db/database.json', JSON.stringify(formData) + '\n', (err) =>{
-            //     if (err) throw err;
-            //     console.log('Form data submitted successfully!')
-            // })
         })   
+
     }
+
+   
 })
 
-server.listen(3000, () => console.log("Server running quite fine"))
+server.listen(3500, () => console.log("Server running quite fine"))
+
